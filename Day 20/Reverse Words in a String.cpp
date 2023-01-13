8053ec7c87df0ba34f16915be9ddba662bce6cac
SUBHAM BANERJEE <90472658+SubhamSatan@users.noreply.github.com>
2023-01-13 12:33:42 +0530

    Create Reverse Words in a String.cpp

---
+++ Day 20/Reverse Words in a String.cpp
@@ -0,0 +1,19 @@
+//Code Studio Reverse Words in a String II
+
+string reverseOrderWords(string s) {
+   string result;
+    int i = 0;
+    int n = s.length();
+
+    while(i < n){
+        while(i < n && s[i] == ' ') i++;
+        if(i >= n) break;
+        int j = i + 1;
+        while(j < n && s[j] != ' ') j++;
+        string sub = s.substr(i, j-i);
+        if(result.length() == 0) result = sub;
+        else result = sub + " " + result;
+        i = j+1;
+    }
+    return result;
+}
