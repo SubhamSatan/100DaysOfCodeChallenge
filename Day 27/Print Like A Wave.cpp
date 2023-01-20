f87ad45ef4463c3d2b35d43f24a9afc8d93ec0f3
SUBHAM BANERJEE <90472658+SubhamSatan@users.noreply.github.com>
2023-01-20 23:35:12 +0530

    Create Print Like A Wave.cpp
    Love Babbar Lecture 23

---
+++ Day 27/Print Like A Wave.cpp
@@ -0,0 +1,26 @@
+//CodeStudio -> Print Like a Wave 
+
+#include <bits/stdc++.h> 
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    //Write your code here
+    vector<int> ans;
+    for(int j = 0;j<mCols;j++)
+    {
+        if(j&1)
+        {
+
+            for(int i = nRows-1;i>=0;i--)
+            {
+                ans.push_back(arr[i][j]);
+            }
+        }
+        else{
+            for(int i = 0;i<nRows;i++)
+            {
+                ans.push_back(arr[i][j]);
+            }
+        }
+    }
+    return ans;
+}
