9f3547d20714bf5888110b585f677a5bb7b23c85
SUBHAM BANERJEE <90472658+SubhamSatan@users.noreply.github.com>
2023-01-23 14:21:39 +0530

    Create Rotate Image .cpp

---
+++ Day 30/Rotate Image .cpp
@@ -0,0 +1,12 @@
+//LeetCode 48 : Rotate Image
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin(), matrix.end());
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+    }
+};
