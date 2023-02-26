b35d78d4eaa3e003a067246d7a74eb86bcf17970
SUBHAM BANERJEE <90472658+SubhamSatan@users.noreply.github.com>
2023-02-26 22:10:13 +0530

    Create Missing Numer.cpp

---
+++ Day 52/Missing Numer.cpp
@@ -0,0 +1,21 @@
+//LeetCode 268 -> Missing Number 
+//Problem Link : https://leetcode.com/problems/missing-number/
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        
+        for(int i = 0;i<nums.size();i++)
+        {
+           if(nums[i] != i)
+           {
+               return i ;
+           }
+        }
+        if(nums[nums.size()-1]!=nums.size())
+        {
+            return nums.size();
+        }
+        return -1;
+    }
+};
