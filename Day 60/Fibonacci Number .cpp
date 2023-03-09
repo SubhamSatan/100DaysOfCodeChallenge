1c946644d0250b7efc8669a5f75cdb9b55acb783
SUBHAM BANERJEE <90472658+SubhamSatan@users.noreply.github.com>
2023-03-09 09:10:27 +0530

    Create Fibonacci Number .cpp

---
+++ Day 60/Fibonacci Number .cpp
@@ -0,0 +1,19 @@
+//LeetCode 509 -> Fibonacci Number 
+//Problem Link : https://leetcode.com/problems/fibonacci-number/
+
+class Solution {
+public:
+    int fib(int n) {
+        
+      if(n == 0)
+      {
+          return 0;
+      }
+      if(n == 1)
+      {
+          return 1;
+      }
+      return fib(n-1) + fib(n-2);
+        
+    }
+};
